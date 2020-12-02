f7144d0af8c713276f9c6738dd04e0749cda9b6d
Seokjin Yoon <sjy5386@gmail.com>
2020-12-02 18:11:08 +0900

    Add script cshPlayerTest

---
+++ Assets/Scripts/cshPlayerTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cshPlayerTest : MonoBehaviour
+{
+    private float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(transform.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(transform.forward * -speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(transform.right * -speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(transform.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(transform.up * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Translate(transform.up * -speed * Time.deltaTime);
+        }
+    }
+}
