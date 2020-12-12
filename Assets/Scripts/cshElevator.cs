e50e569e3629a2c0a5b4630c0d927d9030e2e242
Seokjin Yoon <sjy5386@gmail.com>
2020-12-13 00:03:07 +0900

    Add cshElevator

---
+++ Assets/Scripts/cshElevator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cshElevator : MonoBehaviour
+{
+    public bool sw = false;
+    private float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sw && (transform.position.y < 102.5f))
+        {
+            transform.Translate(transform.up * speed * Time.deltaTime);
+        }
+    }
+}
